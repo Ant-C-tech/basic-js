df167c317b7b9291a4c24e2c291b42008c6ef7b6
Anton Chyvikov <chivikovanton@gmail.com>
2021-01-29 16:15:41 +0200

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  if (members && members.length != 0 && Array.isArray(members)) {
+    const teamName = [];
+    members.forEach((element) => {
+      if (typeof element === "string") {
+        teamName.push(element.trim()[0].toUpperCase());
+      }
+    });
+    return teamName.sort().join("");
+  } else {
+    return false;
+  }
 };
